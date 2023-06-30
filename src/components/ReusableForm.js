d00a52fac6199fb86e0d8d165086ba5056b93c92
Gareth Grindeland <gt.grindeland@gmail.com>
2023-06-30 14:18:13 -0700

    add labels to input fields in reusable form

--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -5,24 +5,36 @@ function ReusableForm(props) {
   return (
     <React.Fragment>
       <form onSubmit={props.formSubmissionHandler}>
-        <input
-          type='text'
-          name='name'
-          placeholder='Coffee Name' />
-        <input
-          type='number'
-          min='0'
-          name='amount'
-          placeholder='Amount (lbs)' />
-        <input
-          type='number'
-          step='0.01'
-          min='0'
-          name='price'
-          placeholder='Price ($)' />
-        <textarea
-          name='description'
-          placeholder='Coffee Description' />
+        <label>
+          Name: 
+          <input
+            type='text'
+            name='name'
+            placeholder="Peets's Coffee Big Bang " />
+        </label><br/>
+        <label>
+          Amount (lbs)
+          <input
+            type='number'
+            min='0'
+            name='amount'
+            placeholder='130' />
+        </label><br/>
+        <label>
+          Price ($) 
+          <input
+            type='number'
+            step='0.01'
+            min='0'
+            name='price'
+            placeholder='8.99' />
+        </label><br/>
+        <label>
+          Description:
+          <textarea
+            name='description'
+            placeholder='Medium Roast, USDA certified organic Latin American beans' />
+        </label><br/>
         <button type='submit'>{props.buttonText}</button>
       </form>
     </React.Fragment>
