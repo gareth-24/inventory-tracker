857a4c237053f54e718edad0b47016724a0ee7db
Gareth Grindeland <gt.grindeland@gmail.com>
2023-06-30 09:56:24 -0700

    add reusable submission form component

---
+++ src/components/ReusableForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function ReusableForm(props) {
+  return (
+    <React.Fragment>
+      <form onSubmit={props.formSubmissionHandler}>
+        <input
+          type='text'
+          name='name'
+          placeholder='Coffee Name' />
+        <input
+          type='number'
+          name='amount'
+          placeholder='Amount' />
+        <input
+          type='number'
+          name='price'
+          placeholder='Price' />
+        <textarea
+          name='description'
+          placeholder='Coffee Description' />
+        <button type='submit'>{props.buttonText}</button>
+      </form>
+    </React.Fragment>
+  );
+}
+
+ReusableForm.propTypes = {
+  formSubmissionHandler: PropTypes.func,
+  buttonText: PropTypes.string
+};
+
+export default ReusableForm;
